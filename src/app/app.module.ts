a5368b6a597f0d909b6b6c2fb4523dfcc126e3ef
Jhonny <jhonnycgarcia@gmail.com>
2024-07-11 01:16:24 -0500

    UP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports: [
     BrowserModule,
     CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
