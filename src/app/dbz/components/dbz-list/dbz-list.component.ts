742a46445c087f3ce50ef697b1787fa1ba77bb89
Jhonny <jhonnycgarcia@gmail.com>
2024-09-14 15:27:16 -0500

    feat: Pensemos en pequeños componentes y Resolución de la tarea - AddCharacter

---
+++ src/app/dbz/components/dbz-list/dbz-list.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './dbz-list.component.html',
+  styleUrl: './dbz-list.component.css',
+})
+export class DbzListComponent { }
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { DbzListComponent } from './components/dbz-list/dbz-list.component';
+import { DbzAddCharacterComponent } from './components/dbz-add-character/dbz-add-character.component';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
+    DbzListComponent,
+    DbzAddCharacterComponent,
   ],
   exports: [
     MainPageComponent,
