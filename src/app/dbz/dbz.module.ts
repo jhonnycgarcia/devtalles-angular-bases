84f415eff669bfa360a4ced3867630030251bb76
Jhonny <jhonnycgarcia@gmail.com>
2024-09-14 22:03:14 -0500

    feat: FormsModule y ngModel

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { DbzListComponent } from './components/dbz-list/dbz-list.component';
 import { DbzAddCharacterComponent } from './components/dbz-add-character/dbz-add-character.component';
-
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { DbzAddCharacterComponent } from './components/dbz-add-character/dbz-add
     MainPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
   ]
 })
 export class DbzModule { }
